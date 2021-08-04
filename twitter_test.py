4cc69a15c9726cbc2d12abc37f4dd09dfe5defc9
agata-bielas <bielas.agata@gmail.com>
2021-08-04 16:55:31 +0200

    Test for function find_hashtags. A lot of code.

--- twitter_test.py
+++ twitter_test.py
@@ -18,3 +18,26 @@ def test_tweet_long_message():
     with pytest.raises(Exception):
         twitter.tweet('test'*41)
     assert twitter.tweets == []
+
+
+def test_tweet_with_hashtag():
+    twitter = Twitter()
+    message = "Test #first message"
+    twitter.tweet(message)
+    assert 'first' in twitter.find_hashtags(message)
+
+
+def test_tweet_with_hashtag_on_beginig():
+    twitter = Twitter()
+    message = "#first test message"
+    twitter.tweet(message)
+    assert "first" in twitter.find_hashtags(message)
+
+
+def test_tweet_with_hashtag_uppercase():
+    twitter = Twitter()
+    message = "#FIRST test message"
+    twitter.tweet(message)
+    assert 'first' in twitter.find_hashtags(message)
+
+
