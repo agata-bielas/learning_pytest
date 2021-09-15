cc280737c96f6f8652005f1d51209cfa63744bae
agata-bielas <bielas.agata@gmail.com>
2021-09-15 17:58:18 +0200

    Decorator pytest.mark.xfail

--- custom_test.py
+++ custom_test.py
@@ -5,7 +5,7 @@ import pytest
 def test_sum():
     assert 2+2 == 4
 
-
+@pytest.mark.xfail
 def test_get_element_from_list():
     custom_list = ["test"]
-    assert custom_list[0] == "test"
+    assert custom_list[0] == "fail"
