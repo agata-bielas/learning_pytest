792922a9f16dd035ea53b5f364a82f350ca99662
agata-bielas <bielas.agata@gmail.com>
2021-10-04 15:50:19 +0200

    Add Direcotry

--- conftest.py
+++ conftest.py
@@ -1,13 +1,14 @@
-import pytest
-
-
-@pytest.fixture(autouse=True)
-def no_requests(monkeypatch):
-    monkeypatch.delattr('requests.sessions.Session.request')
-
-
-@pytest.fixture(name='backend')
-def fixture_backend(tmpdir):
-    temp_file = tmpdir.join('test.txt')
-    temp_file.write('')
-    return temp_file
+# import pytest
+# import requests
+#
+#
+# @pytest.fixture(autouse=True)
+# def no_requests(monkeypatch):
+#     monkeypatch.delattr('requests.sessions.Session.request')
+#
+#
+# @pytest.fixture(name='backend')
+# def fixture_backend(tmpdir):
+#     temp_file = tmpdir.join('test.txt')
+#     temp_file.write('')
+#     return temp_file
