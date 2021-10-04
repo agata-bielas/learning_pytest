9b77b625e82f11fe4602f1b70dd98e3af7feca0e
agata-bielas <bielas.agata@gmail.com>
2021-10-04 21:18:06 +0200

    Refactore of tests. Applied a decorator pytest.mark.parametrize

--- kata/is_anagram_test.py
+++ kata/is_anagram_test.py
@@ -1,16 +1,17 @@
 from is_anagram import is_anagram
+import pytest
 
 
 def test_initial():
     assert is_anagram('test', 'test')
 
 
-def test_not_anagram():
-    assert not is_anagram('test', 'not test')
-
-
-def test_not_anagram2():
-    assert not is_anagram('not test', 'test')
+@pytest.mark.parametrize('message1, message2', (
+        ('test', 'not test'),
+        ('not test', 'test')
+))
+def test_not_anagram(message1, message2):
+    assert not is_anagram(message1, message2)
 
 
 def test_real_anagram():
