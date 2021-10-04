274b8037d98abee953d08de5926f4839e4334566
agata-bielas <bielas.agata@gmail.com>
2021-10-04 21:28:46 +0200

    Add decorator with parameters for test_real_anagram. Refactor function is_anagra

--- kata/is_anagram_test.py
+++ kata/is_anagram_test.py
@@ -15,5 +15,9 @@ def test_not_anagram(message1, message2):
     assert not is_anagram(message1, message2)
 
 
-def test_real_anagram():
-    assert is_anagram('test', 'tets')
+@pytest.mark.parametrize('message1, message2', (
+        ('test', 'tets'),
+        ('cart horse', 'orchestra')
+))
+def test_real_anagram(message1, message2):
+    assert is_anagram(message1, message2)
