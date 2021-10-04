c8a881786479695fc70b358942f36313657d2885
agata-bielas <bielas.agata@gmail.com>
2021-10-04 21:21:09 +0200

    Refator of code. Applied set()

--- kata/is_anagram.py
+++ kata/is_anagram.py
@@ -1,9 +1,3 @@
 
 def is_anagram(message1, message2):
-    for character in message2:
-        if character not in message1:
-            return False
-    for character in message1:
-        if character not in message2:
-            return False
-    return True
+    return set(message1) == set(message2)
